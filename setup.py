74af451ca4f01e56db91d84dd63aabe131e5f841
bedro <bedro@psb.ugent.be>
2020-08-12 10:43:17 +0200

    setup.py improvements

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
 
 setup(
     name='BrAPI2ISA',
+    keywords=["BrAPI", "MIAPPE", "ISA", "Phenotyping", "isa-tab"],
     url='https://github.com/elixir-europe/plant-brapi-to-isa',
+    license='BSD-3',
     packages=['.'],
-    install_requires=open('requirements.txt').read().splitlines(),
-    version='0.1',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=[required],
+    version='1.0',
     description='Convert BrAPI data to ISA',
-    long_description=open('README.md').read(),
+    classifiers=[
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': ["brapi2isa=brapi_to_isa:main"]
+    }
+
 )
